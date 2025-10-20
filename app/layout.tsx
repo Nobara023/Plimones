25de4fa788e5056d0a968f93613d846a0ec5022d
Nobara023 <gomezsarada749@gmail.com>
2025-10-19 21:59:29 -0500

    lili
    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
+ return (
     <html lang="en">
+      <head>
+        {/* 🔍 Script de Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tsx4fusgf1");`,
+          }}
+        />
+      </head>
+
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
