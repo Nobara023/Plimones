82f8c564b4dd1b8a62ad423df42be4551ae3889c
Nobara023 <gomezsarada749@gmail.com>
2025-10-20 15:22:12 -0500

    codi
    codi

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "tsx4fusgf1");`,
+            })(window, document, "clarity", "script", "tsz7zx0f0i");`,
           }}
         />
       </head>
